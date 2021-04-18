2aac72dac4059fc53f727ecca6f79c9381e72efa
Benjamin Brandt <babrandt@gmail.com>
2021-04-18 13:41:29 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ const forecast = require('./utils/forecast.js');
 
 const viewsPath = path.join(__dirname, '..\\templates\\views');
 const partialsPath = path.join(__dirname, '..\\templates\\partials');
-console.log(partialsPath);
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
@@ -94,6 +94,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
